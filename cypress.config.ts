e3000eed622a0725b269cbd51350fbded5b68f10
nahidagithub <148271590+nahidagithub@users.noreply.github.com>
2025-08-01 13:29:28 +0600

    Update cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,1 +1,13 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    supportFile: 'cypress/support/e2e.ts',
+
+    specPattern: 'cypress/e2e/**/*.spec.ts',
+  },
+});
 
